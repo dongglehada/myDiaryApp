3c4aad2efa91c91e37e25de3ec5a49463ba94bd1
= <=>
2023-08-31 19:26:10 +0900

    [FEAT] cell파일 추가

---
+++ myDiaryApp/myDiaryApp/Views/TagCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TagCollectionViewCell.swift
+//  myDiaryApp
+//
+//  Created by SeoJunYoung on 2023/08/31.
+//
+
+import UIKit
+
+class TagCollectionViewCell: UICollectionViewCell {
+    
+}
